46593e773d734c36fa754f105f9dfbc45add0bc4
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2022-08-23 12:36:12 +0500

    Add files via upload

--- invoice_line_field/models/__init__.py
+++ invoice_line_field/models/__init__.py
@@ -2,4 +2,3 @@
 
 from . import models
 from . import invoice_line_field
-from . import  create_invoice_line
--- invoice_line_field/models/invoice_line_field.py
+++ invoice_line_field/models/invoice_line_field.py
@@ -1,14 +1,115 @@
 from odoo import api, fields, models
 
-class AccountMoveModelInherit(models.Model):
-    _inherit = 'account.move'
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def _prepare_invoice_line(self, **optional_values):
+        res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
+        res.update({
+            'sqf': self.square_foot,
+            'quality_id': self.quality_id.id,
+            'quantity': self.product_uom_qty,
+        })
+        return res
 
 
-class InvoiceInheritModel(models.Model):
+class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    quality_id = fields.Many2one("carpet.product.quality")
-    discount = fields.Float('Discount')
-    sqf = fields.Float('Sqf')
+    sqf = fields.Float('SQ Feet', store=True)
+    quality_id = fields.Many2one('carpet.product.quality', 'Quality')
 
 
+    @api.model
+    def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes,
+                                            move_type):
+        ''' This method is used to compute 'price_total' & 'price_subtotal'.
+
+        :param price_unit:  The current price unit.
+        :param quantity:    The current quantity.
+        :param discount:    The current discount.
+        :param currency:    The line's currency.
+        :param product:     The line's product.
+        :param partner:     The line's partner.
+        :param taxes:       The applied taxes.
+        :param move_type:   The type of the move.
+        :return:            A dictionary containing 'price_subtotal' & 'price_total'.
+        '''
+        res = {}
+
+        # Compute 'price_subtotal'.
+        line_discount_price_unit = price_unit * (1 - (discount / 100.0))
+        subtotal = self.sqf * self.quantity
+
+        # Compute 'price_total'.
+        if taxes:
+            taxes_res = taxes._origin.with_context(force_sign=1).compute_all(line_discount_price_unit,
+                                                                             quantity=quantity, currency=currency,
+                                                                             product=product, partner=partner,
+                                                                             is_refund=move_type in (
+                                                                                 'out_refund', 'in_refund'))
+            if self.sqf and self.quantity:
+                res['price_subtotal'] = self.sqf * self.price_unit
+                res['price_total'] = self.sqf * self.price_unit
+        else:
+            res['price_total'] = res['price_subtotal'] = subtotal
+        # In case of multi currency, round before it's use for computing debit credit
+        if currency:
+            res = {k: currency.round(v) for k, v in res.items()}
+        return res
+
+    @api.model
+    def _get_fields_onchange_balance_model(self, quantity, discount, amount_currency, move_type, currency, taxes,
+                                           price_subtotal, force_computation=False):
+        if move_type in self.move_id.get_outbound_types():
+            sign = 1
+        elif move_type in self.move_id.get_inbound_types():
+            sign = -1
+        else:
+            sign = 1
+        amount_currency *= sign
+        if not force_computation and currency.is_zero(amount_currency - price_subtotal):
+            return {}
+
+        taxes = taxes.flatten_taxes_hierarchy()
+        if taxes and any(tax.price_include for tax in taxes):
+            force_sign = -1 if move_type in ('out_invoice', 'in_refund', 'out_receipt') else 1
+            taxes_res = taxes._origin.with_context(force_sign=force_sign).compute_all(amount_currency,
+                                                                                      currency=currency,
+                                                                                      handle_price_include=False)
+            for tax_res in taxes_res['taxes']:
+                tax = self.env['account.tax'].browse(tax_res['id'])
+                if tax.price_include:
+                    amount_currency += tax_res['amount']
+
+        discount_factor = 1 - (discount / 100.0)
+        if amount_currency and discount_factor:
+            # discount != 100%
+            vals = {
+                'quantity': quantity or 1.0,
+                # 'price_unit': self.price_unit,
+            }
+        elif amount_currency and not discount_factor:
+            # discount == 100%
+            vals = {
+                'quantity': quantity or 1.0,
+                'discount': 0.0,
+                # 'price_unit': self.custom_price,
+            }
+        elif not discount_factor:
+            # balance of line is 0, but discount  == 100% so we display the normal unit_price
+            vals = {}
+        else:
+            # balance is 0, so unit price is 0 as well
+            vals = {'price_unit':0.0}
+        return vals
+
+class AccountMoveInherit(models.Model):
+    _inherit = 'account.move'
+
+    def default_get(self, fields_list):
+        res = super(AccountMoveInherit, self).default_get(fields_list)
+        for line in self.invoice_line_ids:
+            line.price_unit =  line.price_unit
+        return  res
